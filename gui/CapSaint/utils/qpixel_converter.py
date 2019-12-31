a28c49156feb4c3d3d8659c1bc26a4f22fe14247
yuetsin <akaza_akari@sjtu.edu.cn>
2019-12-31 17:17:20 +0800

    [fix] missing package names

--- gui/CapSaint/utils/qpixel_converter.py
+++ gui/CapSaint/utils/qpixel_converter.py
@@ -4,6 +4,6 @@ from PyQt5.QtGui import QPixmap, QImage
 def convertToQPixel(im, ratio=1.0):
     size = int(im.size[0] * ratio), int(im.size[1] * ratio)
     
-    im = im.convert("RGBA").resize(size, Image.ANTIALIAS)
+    im = im.convert("RGBA").resize(size, PIL.Image.ANTIALIAS)
     data = im.tobytes("raw","RGBA")
     return QPixmap.fromImage(QImage(data, im.size[0], im.size[1], QImage.Format_RGBA8888))
