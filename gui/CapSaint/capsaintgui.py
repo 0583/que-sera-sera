4b6ec1c90128fe5374dfbccd153998dc0740a90c
yuetsin <akaza_akari@sjtu.edu.cn>
2019-12-31 15:00:44 +0800

    [add] complete main ui

--- gui/CapSaint/capsaintgui.py
+++ gui/CapSaint/capsaintgui.py
@@ -1,15 +1,16 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import mainwindow
+from PyQt5 import QtWidgets
 
-
-class CapSaintGui(QMainWindow):
+class CapSaintGui(QtWidgets.QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
-
+        app = QtWidgets.QApplication(sys.argv)
+        MainWindow = QtWidgets.QMainWindow()
+        ui = mainwindow.Ui_MainWindow()
+        ui.setupUi(MainWindow)
+        MainWindow.show()
+        sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = CapSaintGui()
-    window.show()
-    sys.exit(app.exec_())
+    CapSaintGui()
