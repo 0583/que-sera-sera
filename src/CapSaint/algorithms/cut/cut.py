f429ee53c202668e9980ce16ea0b810608b71d80
RoyalRaisins <40791674+RoyalRaisins@users.noreply.github.com>
2020-01-01 16:20:12 +0800

    Update cut.py

--- src/CapSaint/algorithms/cut/cut.py
+++ src/CapSaint/algorithms/cut/cut.py
@@ -6,16 +6,11 @@ def cut(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
     h, w = thresh.shape[0:2]
-    closed_r = cv2.resize(thresh, (int(w/3), int(h/3)))
-    cv2.imshow("enhanced", closed_r)
-    cv2.waitKey(0)
     mask = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15))
     closed = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, mask)
     closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
     h, w = closed.shape[0:2]
-    closed_r = cv2.resize(closed, (int(w/3), int(h/4)))
-    cv2.imshow("enhanced", closed_r)
     x = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
                          cv2.CHAIN_APPROX_NONE)
     cnts, _b = x
